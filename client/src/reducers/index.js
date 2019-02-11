79736412af6b4bdc11635d0266441ef7359fa7cf
Akash Dobaria <akashdobaria@Akashs-MacBook-Pro.local>
2019-02-10 20:14:05 -0600

    starting to use git for the project

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { reducer as reduxFormReducer } from 'redux-form';
+import authReducer from './authReducer';
+import streamReducer from './streamReducers';
+
+export default combineReducers({
+    auth: authReducer,
+    form: reduxFormReducer,
+    streams: streamReducer
+});
